79b008ff684058e64107a11295fd52eafd76a9d2
mgroth0 <mgroth49@gmail.com>
2022-06-12 16:17:04 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
-
-
 @file:Suppress("UnstableApiUsage")
+
 modtype = LIB
+
+apis(
+  project(":k:klib")
+)
+
 dependencies {
-//  api(projects.kj.kjlib)
-//  api(projects.kj.async)
-//  api(libs.gson)
+  //  api(projects.kj.kjlib)
+  //  api(projects.kj.async)
+  //  api(libs.gson)
   api(libs.kotlinx.serialization.json)
   implementation(libs.fx.base)
-//  implementation(projects.kj.reflect)
-  projectOrLocalMavenJVM("api", ":k:klib")
+  //  implementation(projects.kj.reflect)
+  //  projectOrLocalMavenJVM("api", ":k:klib")
   implementation(kotlin("reflect"))
 }
 
