99b4baef84d2b4be1b47ab12845ee10945214b00
mgroth0 <mgroth49@gmail.com>
2022-07-06 14:14:24 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,14 @@
 
 modtype = LIB
 
-apis(
-  ":k:klib".jvm(),
-  ":k:file".jvm()
-)
+apis {
+  klib
+  file
+}
 
 dependencies {
-  //  api(projects.k.kjlib)
-  //  api(projects.k.async)
-  //  api(libs.gson)
   api(libs.kotlinx.serialization.json)
   implementation(libs.fx.base)
-  //  implementation(projects.k.reflect)
-  //  projectOrLocalMavenJVM("api", ":k:klib")
   implementation(kotlin("reflect"))
 }
 
