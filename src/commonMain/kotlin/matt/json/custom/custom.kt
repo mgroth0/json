284645f094e29a7ce274cd58deca7964d2453e37
mgroth0 <mgroth49@gmail.com>
2022-09-03 17:25:26 -0400

    autocommit

--- src/commonMain/kotlin/matt/json/custom/custom.kt
+++ src/commonMain/kotlin/matt/json/custom/custom.kt
@@ -27,7 +27,7 @@ import matt.json.custom.JsonWriter.MapJsonWriter
 import matt.json.custom.JsonWriter.NumberJsonWriter
 import matt.json.custom.JsonWriter.StringJsonWriter
 import matt.json.klaxon.Render
-import matt.klib.boild.Builder
+import matt.model.boild.Builder
 import kotlin.jvm.JvmName
 import kotlin.reflect.KProperty
 
