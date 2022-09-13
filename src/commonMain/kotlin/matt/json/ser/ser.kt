13f7b229e9a09cd99996fefea3022a444de1177a
mgroth0 <mgroth49@gmail.com>
2022-09-13 11:27:03 -0400

    autocommit

--- src/commonMain/kotlin/matt/json/ser/ser.kt
+++ src/commonMain/kotlin/matt/json/ser/ser.kt
@@ -7,8 +7,10 @@ import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import kotlin.reflect.KClass
 
 
@@ -32,6 +34,12 @@ abstract class JsonArraySerializer<T: Any>(cls: KClass<T>): MySerializer<T>(cls)
   final override fun deserialize(jsonElement: JsonElement): T = deserialize(jsonElement.jsonArray)
   abstract fun deserialize(jsonArray: JsonArray): T
   abstract override fun serialize(value: T): JsonArray
+}
+
+abstract class JsonPrimitiveSerializer<T: Any>(cls: KClass<T>): MySerializer<T>(cls) {
+  final override fun deserialize(jsonElement: JsonElement): T = deserialize(jsonElement.jsonPrimitive)
+  abstract fun deserialize(jsonPrimitive: JsonPrimitive): T
+  abstract override fun serialize(value: T): JsonPrimitive
 }
 
 
