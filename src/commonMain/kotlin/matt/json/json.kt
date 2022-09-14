51d47fc8724757195cf657317b03e191864c09e0
mgroth0 <mgroth49@gmail.com>
2022-09-14 09:40:50 -0400

    autocommit

--- src/commonMain/kotlin/matt/json/json.kt
+++ src/commonMain/kotlin/matt/json/json.kt
@@ -104,4 +104,5 @@ fun <T> DeserializationStrategy<T>.withSerializationStrategy(s: Encoder.(T)->Uni
 inline fun <reified T> T.toJson() = Json.encodeToJsonElement(this)
 
 inline fun <reified T> T.toJsonString() = Json.encodeToString(this)
+const val yesIUseJsonButAnInlineFunSoItDoesntShowInBytecode = "yesIUseJsonButAnInlineFunSoItDoesntShowInBytecode"
 
