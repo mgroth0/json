e56bd91fb27acf24b82b428498998a6903778a82
mgroth0 <mgroth49@gmail.com>
2023-02-06 23:30:44 -0500

    autocommit

--- src/jsMain/kotlin/matt/json/ser/ser.kt
+++ src/jsMain/kotlin/matt/json/ser/ser.kt
@@ -6,12 +6,10 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonElement
 import matt.model.code.idea.SerIdea
-import matt.reflect.isSubTypeOf
 import kotlin.reflect.KClass
 
 /*cls used to be qname: String, but this is much less typesafe*/
-actual abstract class MyJsonSerializer<T: Any> actual constructor(cls: KClass<*>): KSerializer<T>, SerIdea {
-  val cls = cls
+actual abstract class MyJsonSerializer<T: Any> actual constructor(val cls: KClass<*>): KSerializer<T>, SerIdea {
   actual final override val descriptor: SerialDescriptor get() = TODO("Not yet implemented")
   actual final override fun deserialize(decoder: Decoder): T {
 	TODO("Not yet implemented")
@@ -23,7 +21,7 @@ actual abstract class MyJsonSerializer<T: Any> actual constructor(cls: KClass<*>
   actual abstract fun serialize(value: T): JsonElement
 
   actual fun canSerialize(value: Any): Boolean {
-	return value::class.isSubTypeOf(cls)
+	return cls.isInstance(value)/*value::class.isSubTypeOf(cls)*/
   }
 
   actual fun castAndSerialize(value: Any): JsonElement {
