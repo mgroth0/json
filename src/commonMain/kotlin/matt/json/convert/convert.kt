143852d21b9f490bfda7de4b91ad63bf363f7bb0
mgroth0 <mgroth49@gmail.com>
2023-03-03 12:35:12 -0500

    autocommit

---
+++ src/commonMain/kotlin/matt/json/convert/convert.kt
@@ -0,0 +1,19 @@
+package matt.json.convert
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+import matt.model.op.convert.StringConverter
+
+
+class JsonConverter<T>(
+  private val ser: KSerializer<T>,
+  private val json: Json = Json,
+): StringConverter<T> {
+  override fun toString(t: T): String {
+	return json.encodeToString(ser, t)
+  }
+
+  override fun fromString(s: String): T {
+	return json.decodeFromString(ser, s)
+  }
+}
