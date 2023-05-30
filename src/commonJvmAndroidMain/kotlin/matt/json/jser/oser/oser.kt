15079fb6adf216885ee26fee9389cc099f5bf964
mgroth0 <mgroth49@gmail.com>
2023-05-29 22:58:49 -0400

    changed src/commonJvmAndroidMain/kotlin/matt/json/jser/oser/oser.kt

--- src/commonJvmAndroidMain/kotlin/matt/json/jser/oser/oser.kt
+++ src/commonJvmAndroidMain/kotlin/matt/json/jser/oser/oser.kt
@@ -9,25 +9,15 @@ import java.io.Serializable
 @SeeURL("https://github.com/gradle/gradle/issues/13588")
 interface JavaIoSerializable : Serializable {
 
-//    abstract val thisCls: KClass<T>
-//    abstract val thisRef: T
-
-
     private fun writeObject(out: ObjectOutputStream) {
-//        out.writeJson(thisCls, thisRef)
         out.defaultWriteObject()
     }
 
-//    private var weirdProxyThis: T? = null
 
     private fun readObject(`in`: ObjectInputStream) {
-//        weirdProxyThis = `in`.readJson(thisCls)
         `in`.defaultReadObject()
     }
 
-//    fun readResolve(o: T): Any {
-//        return weirdProxyThis!!
-//    }
 
 }
 
